d6dd2df7ad7176628bc449f79746f5c3c0ac0f2a
Deborah Scordo <d.scordo.mackie@gmail.com>
2022-06-20 12:52:36 +0200

    added pinia

--- client/src/main.js
+++ client/src/main.js
@@ -1,11 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css"
 import { createApp } from 'vue'
+import { createPinia } from 'pinia';
 import App from './App.vue'
 import router from './router'
 
 
 
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(createPinia()).mount('#app')
 
 import "bootstrap/dist/js/bootstrap.js"
